8d7d2ea0e5836ced8e6d3391abb6b9a4b2a9a009
OTerrien <oterrien@neuf.fr>
2017-11-11 18:12:15 +0100

    add transparent background

--- user-rights-domain/src/main/java/com/ote/domain/secret/business/SecretService.java
+++ user-rights-domain/src/main/java/com/ote/domain/secret/business/SecretService.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public final class SecretService implements ISecretService {
 
-    // region Factory
+    // region Factory...
     @Getter
     private static final Factory Factory = new Factory();
 
